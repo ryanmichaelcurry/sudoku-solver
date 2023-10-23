9e32ad5fe444e64da69538e9e48f29d7f5085fe7
Ryan Michael Curry <ryanmichaelcurry@gmail.com>
2023-10-23 11:20:22 -0500

    almost done

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,33 @@
+/*
+Author: Ryan Michael Curry
+Sources:
+https://www.thonky.com/sudoku/evaluate-sudoku
+https://www.yagiz.co/sudoku-generating-valid-one
+*/
 use rand::seq::SliceRandom;
 use rand::thread_rng;
-use rand::Rng;
 use std::thread;
 use std::time::Duration;
 use std::env;
 
 //print!("\x1B[2J\x1B[H"); // ChatGPT :)
-
+fn sudoku_string(board: &Vec<Vec<usize>>) {
+    for row in board {
+        for value in row {
+            if (*value as i32) == 0 {
+                print!(".");
+            } else {
+                print!("{}", value);
+            }
+        }
+    }
+    println!("\n");
+}
 fn print_sudoku_board(board: &Vec<Vec<usize>>, size: usize) {
     let sqrt_size = (size as f32).sqrt() as usize;
     let mut output = String::new();
+
+    print!("\x1B[2J\x1B[H"); // ChatGPT :)
 
     for (i, row) in board.iter().enumerate() {
         if i % sqrt_size == 0 && i != 0 {
@@ -57,8 +75,8 @@ fn is_valid(board: &Vec<Vec<usize>>, size: usize, x: usize, y: usize, value: usi
     let sqrt_size = (size as f64).sqrt() as usize;
     let x_start = (x / sqrt_size) * sqrt_size; // inner square x (for 9x9 its either 0 1 2)
     let y_start = (y / sqrt_size) * sqrt_size; // inner square y (for 9x9 its either 0 1 2)
-    for i in x_start..sqrt_size {
-        for j in y_start..sqrt_size {
+    for i in x_start..x_start + sqrt_size {
+        for j in y_start..y_start + sqrt_size {
             if board[i][j] == value {
                 return false;
             }
@@ -150,19 +168,118 @@ fn sudoku_solver(
     return false;
 }
 
-fn anakin(board: &mut Vec<Vec<usize>>, size: usize, difficulty: usize) {
+fn collect_sudoku_solutions(
+    board: &mut Vec<Vec<usize>>,
+    size: usize,
+    x: usize,
+    y: usize,
+    solutions: &mut Vec<Vec<Vec<usize>>>
+) {
+    if x == size {
+        solutions.push(board.clone());
+        return;
+    }
+
+    if board[x][y] != 0 {
+        if y == size - 1 {
+            collect_sudoku_solutions(board, size, x + 1, 0, solutions);
+        } else {
+            collect_sudoku_solutions(board, size, x, y + 1, solutions);
+        }
+        return;
+    }
+
+    let mut rng = thread_rng();
+    let mut available_positions = find_available_positions(board, size, x, y);
+
+    // Shuffle available positions for randomness
+    available_positions.shuffle(&mut rng);
+
+    for &value in &available_positions {
+        board[x][y] = value as usize;
+        if y == size - 1 {
+            collect_sudoku_solutions(board, size, x + 1, 0, solutions);
+        } else {
+            collect_sudoku_solutions(board, size, x, y + 1, solutions);
+        }
+    }
+
+    // If no position is available, backtrack
+    board[x][y] = 0;
+}
+
+fn sudoku_unique(board: &mut Vec<Vec<usize>>, size: usize) -> bool {
+    let mut solutions = vec![];
+    collect_sudoku_solutions(board, size, 0, 0, &mut solutions);
+    return solutions.len() == 1;
+}
+
+fn generate_unique(board: &mut Vec<Vec<usize>>, size: usize, difficulty: usize) {
     let mut eraser: i32 = (difficulty * size) as i32;
-    if eraser > (size * size) as i32 {
+    if eraser > ((size * size) as i32) {
         eraser = (size * size) as i32;
     }
 
-    while eraser > 0 {
-        let x = rand::thread_rng().gen_range(0..size);
-        let y = rand::thread_rng().gen_range(0..size);
-        if board[x][y] != 0 {
-            board[x][y] = 0;
-            eraser -= 1;
+    let mut not_visited: Vec<Vec<usize>> = vec![];
+    for x in 0..size {
+        for y in 0..size {
+            not_visited.push(vec![x, y]);
         }
+    }
+    /*
+    for cell in &not_visited {
+        for pos in cell {
+            print!("{} ", pos);
+        }
+        println!();
+    }
+    */
+    not_visited.shuffle(&mut thread_rng());
+
+    while eraser > 0 {
+        //println!("{}", not_visited.len());
+        if not_visited.len() > 0 {
+            let temp = board[not_visited[0][0]][not_visited[0][1]];
+            board[not_visited[0][0]][not_visited[0][1]] = 0;
+            if !sudoku_unique(board, size) {
+                board[not_visited[0][0]][not_visited[0][1]] = temp;
+                not_visited.drain(0..1);
+            } else {
+                not_visited.drain(0..1);
+                eraser -= 1;
+            }
+        } else {
+            return;
+        }
+    }
+}
+
+fn anakin(board: &mut Vec<Vec<usize>>, size: usize, difficulty: usize) {
+    let mut eraser: i32 = (difficulty * size) as i32;
+    if eraser > ((size * size) as i32) {
+        eraser = (size * size) as i32;
+    }
+
+    let mut not_visited: Vec<Vec<usize>> = vec![];
+    for x in 0..size {
+        for y in 0..size {
+            not_visited.push(vec![x, y]);
+        }
+    }
+
+    /*
+    for cell in &not_visited {
+        for pos in cell {
+            print!("{} ", pos);
+        }
+        println!();
+    }
+    */
+    not_visited.shuffle(&mut thread_rng());
+    not_visited.drain(0..((size * size) - eraser as usize));
+
+    for position in not_visited {
+        board[position[0]][position[1]] = 0;
     }
 }
 
@@ -212,6 +329,7 @@ fn main() {
 
     anakin(&mut board, size, difficulty);
     print_sudoku_board(&board, size);
+    sudoku_string(&board);
 
     sudoku_solver(&mut board, size, 0, 0, melatonin);
     print_sudoku_board(&board, size);
---
+++ target/release/sudoku.d
@@ -0,0 +1,1 @@
+/home/frodo/msstate/sudoku-solver/target/release/sudoku: /home/frodo/msstate/sudoku-solver/src/main.rs
